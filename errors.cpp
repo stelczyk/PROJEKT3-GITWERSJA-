6931f355df62fb87e502e0dcf146cdae418278fa
stelczyk <121972876+stelczyk@users.noreply.github.com>
2024-12-02 16:50:21 +0100

    Update errors.cpp

--- errors.cpp
+++ errors.cpp
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Funkcja do obs³ugi b³êdów otwierania pliku
+// Funkcja do obslugi bledÃ³w otwierania pliku
 void handle_file_error(FILE* file, const char* filename) {
     if (!file) {
-        fprintf(stderr, "B³¹d: Nie mo¿na otworzyæ pliku %s.\n", filename);
+        fprintf(stderr, "Blad: Nie mozna otworzyc pliku %s.\n", filename);
         exit(EXIT_FAILURE);
     }
 }
 
-// Funkcja do obs³ugi b³êdów alokacji pamiêci
+// Funkcja do obslugi bledÃ³w alokacji pamiÃªci
 void handle_memory_error(void* ptr, const char* variable_name) {
     if (!ptr) {
-        fprintf(stderr, "B³¹d: Nie uda³o siê zaalokowaæ pamiêci dla %s.\n", variable_name);
+        fprintf(stderr, "Blad: Nie udalo sie zaalokowac pamieci dla %s.\n", variable_name);
         exit(EXIT_FAILURE);
     }
 }
 
-// Funkcja do obs³ugi niepoprawnego indeksu
+// Funkcja do obslugi niepoprawnego indeksu
 void handle_invalid_index(int index, int max_index) {
     if (index < 0 || index >= max_index) {
-        fprintf(stderr, "B³¹d: Niepoprawny indeks %d (dozwolone 0-%d).\n", index, max_index - 1);
+        fprintf(stderr, "Blad: Niepoprawny indeks  (dozwolone 0-%d).\n", max_index - 1);
         exit(EXIT_FAILURE);
     }
 }
